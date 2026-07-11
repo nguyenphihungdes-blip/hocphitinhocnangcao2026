86324820503c51871eb2c97a2ad37a3190121de0
nguyenphihungdes-blip <nguyenphihungdes@gmail.com>
2026-07-11 09:54:57 +0700

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const APPS_SCRIPT_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwikncbbcz7xrORA5ihTGy7m3mKYXlX6CducmY2HWzaKzK-aHPbvcIEh8NjOIhdCk7a/exec";
 const MAX_FILE_SIZE = 8 * 1024 * 1024;
 
 const studentsByClass = {
